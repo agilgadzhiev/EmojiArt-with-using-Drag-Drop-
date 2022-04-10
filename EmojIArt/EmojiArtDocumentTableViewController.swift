8c7e19a0a8b2052e02e546ab1ec33861de588b8c
agil_gadzhiev <878agil878@gmail.com>
2022-04-11 02:38:06 +0300

    edit initial VC, add deleting rows in TVC and adding new rows with barButton

--- EmojIArt/EmojiArtDocumentTableViewController.swift
+++ EmojIArt/EmojiArtDocumentTableViewController.swift
@@ -9,24 +9,39 @@ import UIKit
 
 class EmojiArtDocumentTableViewController: UITableViewController {
 
-
+    var emojiArtDocuments = ["One", "Two", "Three"]
+    
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return emojiArtDocuments.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell", for: indexPath)
+        
+        cell.textLabel?.text = emojiArtDocuments[indexPath.row]
+        
 
         return cell
+    }
+    
+    @IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
+        emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            emojiArtDocuments.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
     
 
