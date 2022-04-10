dee0090606029ddf0f9b02cf3cd7506aeb98a629
agil_gadzhiev <878agil878@gmail.com>
2022-04-10 22:00:52 +0300

    initial commit

---
+++ EmojIArt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojIArt
+//
+//  Created by Агил Гаджиев on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
